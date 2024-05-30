e9c59241563ef23ed99d0ce06cfad69108cb0385
Divin_KL <divinlonji@gmail.com>
2024-05-30 04:29:35 +0200

    finance + fleet

---
+++ finance/eg_asset_management/models/asset_category.py
@@ -0,0 +1,8 @@
+from thrive import fields, models, api
+
+
+class AssetCategory(models.Model):
+    _name = "asset.category"
+    _description = "Asset Category"
+
+    name = fields.Char(string="Name")
